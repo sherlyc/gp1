9cf87994971e21811b6d875167bcc58c0faff92b
anna-marie-vos <amvos@outlook.com>
2017-05-05 10:17:26 +1200

    this was all sherley

---
+++ functions.js
@@ -0,0 +1,24 @@
+const data = require('./data.json')
+
+console.log({data});
+
+sortPrioirity = (data) => {
+  let high = filterPriority(data, 'High')
+  let medium = filterPriority(data, 'Medium')
+  let low = filterPriority(data, 'Low')
+  console.log({high, medium, low});
+  let sorted = high.concat(medium.concat(low))
+  console.log({sorted});
+}
+
+filterPriority = (data, priority) => {
+  return data.filter((item) => {
+    return item.priority == priority
+  })
+}
+
+sortPrioirity(data)
+
+module.exports = {
+  sortPrioirity
+}
