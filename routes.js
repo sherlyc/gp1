d3aeb181a4e303ae2922418688034b31888e13d0
anna-marie-vos <amvos@outlook.com>
2017-05-05 11:48:48 +1200

    adding task from form correclty

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+var funcs = require('./functions')
+var data = require('./data.json')
 
 router.get('/', (req, res) => {
   res.render('home')
@@ -8,4 +10,14 @@ router.get('/', (req, res) => {
 router.get('/other', (req, res) => {
   res.render('other')
 })
+
+router.post('/', (req, res) => {
+  // console.log(req.body.newTask)
+  console.log(req.body);
+  funcs.addTask(req.body)
+  console.log(data)
+
+})
+
+
 module.exports = router
